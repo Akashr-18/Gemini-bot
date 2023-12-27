582fda58cee4521b03cef5af7a359261062b2c96
Akashr-18 <akash.ravichandran18@gmail.com>
2023-12-27 12:06:56 +0530

    bot file updated

---
+++ bot.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title='Q&A demo')
+st.header("Gemini Application")
+
+input = st.text_input("Input: ", key='input')
+button = st.button('Submit: ')
+
+if input and button:
+    response = get_gemini_response(input)
+    st.subheader("Response: ")
+    st.write(response)
